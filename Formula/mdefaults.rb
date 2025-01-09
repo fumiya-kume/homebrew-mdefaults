eac833375a89dd6994f61a0f124d6d4f9b9a3d78
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2025-01-09 19:58:15 +0900

    Update mdefaults.rb

--- Formula/mdefaults.rb
+++ Formula/mdefaults.rb
@@ -1,7 +1,7 @@
 class Mdefaults < Formula
   desc "macOS configuration management tool like HomeBrew Bundle"
   homepage "https://github.com/fumiya-kume/mdefaults"
-
+  version "0.0.47"
   sha256 "2781d96c9efde569901c0f99d7395759397e4d20b4767685be31fe49147afa48"
   license "MIT"
   head "https://github.com/fumiya-kume/mdefaults.git", branch: "master"
@@ -10,7 +10,7 @@ class Mdefaults < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/fumiya-kume/mdefaults/releases/download/v0.0.47/mdefaults-arm64-v0.0.47.zip"
+      url "https://github.com/fumiya-kume/mdefaults/releases/download/v#{version}/mdefaults-arm64-v#{version}.zip"
       sha256 "279d2b09486a511173912d903e4fd4ec86a3d64742fde5bb5f2a38063b7bb9d4"
 
       def install
@@ -18,7 +18,7 @@ class Mdefaults < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/fumiya-kume/mdefaults/releases/download/v0.0.47/mdefaults-amd64-v0.0.47.zip"
+      url "https://github.com/fumiya-kume/mdefaults/releases/download/v#{version}/mdefaults-amd64-v#{version}.zip"
       sha256 "2781d96c9efde569901c0f99d7395759397e4d20b4767685be31fe49147afa48"
 
       def install

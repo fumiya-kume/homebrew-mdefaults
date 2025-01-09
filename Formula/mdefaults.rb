29aec107b59d1f1658f88260477819bdb776b109
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2025-01-09 20:39:29 +0900

    Try to simple flow

--- Formula/mdefaults.rb
+++ Formula/mdefaults.rb
@@ -20,21 +20,8 @@ class Mdefaults < Formula
   depends_on "go" => :build
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/fumiya-kume/mdefaults/releases/download/v#{HOMEBREW_GITPULLS_VERSION}/mdefaults-arm64-v#{HOMEBREW_GITPULLS_VERSION}.zip"
-      sha256 "279d2b09486a511173912d903e4fd4ec86a3d64742fde5bb5f2a38063b7bb9d4"
-
-      def install
-        bin.install "mdefaults"
-      end
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/fumiya-kume/mdefaults/releases/download/v#{HOMEBREW_GITPULLS_VERSION}/mdefaults-amd64-v#{HOMEBREW_GITPULLS_VERSION}.zip"
-      sha256 "2781d96c9efde569901c0f99d7395759397e4d20b4767685be31fe49147afa48"
-
-      def install
-        bin.install "mdefaults"
-      end
+    def install
+      system "go", "build", *std_go_args(ldflags: "-s -w"), "./mdefaults"
     end
   end
 

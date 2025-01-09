1a71ac5b18756f4400ca1a21df04e22239422436
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2025-01-09 19:48:20 +0900

    Update mdefaults.rb

--- Formula/mdefaults.rb
+++ Formula/mdefaults.rb
@@ -1,7 +1,7 @@
 class Mdefaults < Formula
   desc "macOS configuration management tool like HomeBrew Bundle"
   homepage "https://github.com/fumiya-kume/mdefaults"
-  url "https://github.com/fumiya-kume/mdefaults/releases/download/v0.0.47/mdefaults-amd64-v0.0.47.zip"
+
   sha256 "2781d96c9efde569901c0f99d7395759397e4d20b4767685be31fe49147afa48"
   license "MIT"
   head "https://github.com/fumiya-kume/mdefaults.git", branch: "master"
@@ -9,6 +9,22 @@ class Mdefaults < Formula
   depends_on "go" => :build
 
   on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/fumiya-kume/mdefaults/releases/download/v0.0.47/mdefaults-arm64-v0.0.47.zip"
+      sha256 "29573d65dc9395cc89be0c6e4576e44c04669cb157c3d89472ae2344ceecd7ad"
+
+      def install
+        bin.install "mdefaults"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/fumiya-kume/mdefaults/releases/download/v0.0.47/mdefaults-amd64-v0.0.47.zip"
+      "2781d96c9efde569901c0f99d7395759397e4d20b4767685be31fe49147afa48"
+
+      def install
+        bin.install "mdefaults"
+      end
+    end
     def install
       bin.install "mdefaults"
     end

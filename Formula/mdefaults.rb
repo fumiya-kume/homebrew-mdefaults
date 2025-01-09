391d3f56a578ffe06da434be87db790edd28ad47
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2025-01-09 19:03:35 +0900

    Update mdefaults.rb

--- Formula/mdefaults.rb
+++ Formula/mdefaults.rb
@@ -4,6 +4,7 @@ class Mdefaults < Formula
   url "https://github.com/fumiya-kume/mdefaults/releases/download/v0.0.47/mdefaults-amd64-v0.0.47.zip"
   sha256 "2781d96c9efde569901c0f99d7395759397e4d20b4767685be31fe49147afa48"
   license "MIT"
+  head "https://github.com/fumiya-kume/mdefaults.git", branch: "master"
 
   depends_on "go" => :build
 

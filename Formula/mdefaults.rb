a76ff7eb5734c5b960a3f478aba7a5b9873da12b
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2025-01-09 18:50:14 +0900

    Update mdefaults.rb

--- Formula/mdefaults.rb
+++ Formula/mdefaults.rb
@@ -1,6 +1,6 @@
 class Mdefaults < Formula
-  desc ""
-  homepage ""
+  desc "macOS configuration management tool, which store in the ~/.mdefaults like HomeBrew Bundle"
+  homepage "github.com/fumiya-kume/mdefaults"
   url "https://github.com/fumiya-kume/mdefaults/releases/download/v0.0.47/mdefaults-amd64-v0.0.47.zip"
   sha256 "2781d96c9efde569901c0f99d7395759397e4d20b4767685be31fe49147afa48"
   license "GPL-3.0"

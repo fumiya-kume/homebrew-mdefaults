6b94b8513995629e909d1ce8543a935ff71a5646
kuu(Fumiya Kume) <fumiya.kume@hotmail.com>
2025-01-09 19:50:03 +0900

    Update mdefaults.rb

--- Formula/mdefaults.rb
+++ Formula/mdefaults.rb
@@ -11,7 +11,7 @@ class Mdefaults < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/fumiya-kume/mdefaults/releases/download/v0.0.47/mdefaults-arm64-v0.0.47.zip"
-      sha256 "2781d96c9efde569901c0f99d7395759397e4d20b4767685be31fe49147afa48"
+      sha256 "411f29d5ed02a40a4b2a9960edca644814410fdaa54427be887b9b0ec28c1711"
 
       def install
         bin.install "mdefaults"
